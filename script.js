816d6b39cacfa61048c9283f3983d58d68f4f2a5
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-01-24 14:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS code here.
+const sliderImages=document.querySelectorAll(".slide-in");
+
+
+function checkSlide(e) {
+	console.count(e);
+sliderImages.forEach(slideImage=>{
+const slideInAt=(window.scrollY+window.innerHeight)-slideImage.height/2;
+const imageBotton=sliderImage.offsetTop+slideImage.height;
+	const in HalfShown=slideInAt>sliderImage.offsetTop;
+	const isNotScrolledFast=window.scrollY<imageBotton;
+	if(isHalfShown && isNotScrolledPast){
+slideImage.classList.add("active");
+		
+
+		
+	}
+else{
+slideImage.classList.remove("active");
+	
+}
+	
+})
+
+	
+}
+window.addEventListener("scroll",debounce(checkSlide));
